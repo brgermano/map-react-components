c29dde9195a6de9054faa49d4cc7d1b1b21fde9f
Bruno Alves Germano Pinto <bgp@smartbank.com.br>
2019-10-09 08:58:30 -0300

    Remove files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Familia from './componentes/Familia'
+import Membro from './componentes/Membro'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const elemento = document.getElementById('root')
+ReactDOM.render(
+    <div>
+        <Familia sobrenome="Pereira">
+            <Membro nome="Andre" />
+            <Membro nome="Mariana" />
+        </Familia>
+    </div>
+, elemento)
+
+
+
+
+// const jsx = <h1>Olá React!</h1>
+// ReactDOM.render(jsx, elemento)
+
+
+// ReactDOM.render(
+//     <ul>
+//         <li>1) Pedro</li>
+//         <li>2) Maria</li>
+//         <li>3) Ana</li>
+//     </ul>
+// , elemento)
+
+// const lista = document.createElement('ul')
+
+// let item = document.createElement('li')
+// let texto = document.createTextNode('1) Pedro')
+// item.appendChild(texto)
+// lista.appendChild(item)
+
+// item = document.createElement('li')
+// texto = document.createTextNode('2) Maria')
+// item.appendChild(texto)
+// lista.appendChild(item)
+
+// item = document.createElement('li')
+// texto = document.createTextNode('3) Ana')
+// item.appendChild(texto)
+// lista.appendChild(item)
+
+// const elemento = document.getElementById('root')
+// elemento.appendChild(lista)
